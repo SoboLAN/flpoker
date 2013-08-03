59f7de27bb9a01bdad75e76b08fc74de919004e2
Radu Murzea <radu.murzea@gmail.com>
2013-08-03 22:23:58 +0300

    Added site-wide configuration file

---
+++ config/site.config.php
@@ -0,0 +1,13 @@
+<?php
+	$siteConfig = array (
+		'online' => true
+		'online_eta' => '0 hours',
+		'default_lang' => 'ro',
+		'lang_cookie_duration' => 30 * 24 * 3600,
+		'cache_lifetime_classic_view' => 12 * 3600,
+		'cache_lifetime_top_six_months' => 12 * 3600,
+		'cache_lifetime_top_alltime' => 12 * 3600,
+		'cache_lifetime_top_bonus' => 24 * 3600,
+		'cache_lifetime_five' => 12 * 3600,
+	);
+	
