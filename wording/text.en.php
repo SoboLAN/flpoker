67a92d177fe85c2a5ec351dd5827d7956518fa91
Radu Murzea <radu.murzea@gmail.com>
2013-08-04 14:03:45 +0300

    - Added some more labels

--- wording/text.en.php
+++ wording/text.en.php
@@ -2,6 +2,15 @@
 	$siteLabels = array (
 		'menu_home' => 'Home Page',
 		'menu_classic' => 'Classic View',
-		'menu_stats' => 'Statistics',
+		'menu_statistics' => 'Statistics',
+		'menu_players' => 'Players',
+		'menu_tournaments' => 'Tournaments',
+		'menu_rules' => 'Rules',
+		'menu_askprize' => 'Ask for Prize',
+		'menu_contact' => 'Contact',
+		'langpanel_ro' => 'Romanian',
+		'langpanel_ro_switch' => 'Switch to Romanian',
+		'langpanel_en' => 'English',
+		'langpanel_en_switch' => 'Switch to English'
 		
 	);
--- wording/text.ro.php
+++ wording/text.ro.php
@@ -2,6 +2,15 @@
 	$siteLabels = array (
 		'menu_home' => 'Acasă',
 		'menu_classic' => 'Aspect Clasic',
-		'menu_stats' => 'Statistici',
+		'menu_statistics' => 'Statistici',
+		'menu_players' => 'Jucători',
+		'menu_tournaments' => 'Turnee',
+		'menu_rules' => 'Reguli',
+		'menu_askprize' => 'Cere Premiu',
+		'menu_contact' => 'Contact',
+		'langpanel_ro' => 'Română',
+		'langpanel_ro_switch' => 'Schimbă în Română',
+		'langpanel_en' => 'Engleză',
+		'langpanel_en_switch' => 'Schimbă în Engleză'
 		
 	);
