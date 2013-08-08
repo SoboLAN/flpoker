7fe94a34534ea23a4c697e8032db749f914f3dd1
Radu Murzea <radu.murzea@gmail.com>
2013-08-08 22:16:18 +0300

    - Index page and language switch changes.

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+	require_once 'Site.class.php';
+	
+	$site = new Site();
+	
+	$htmlout = $site->getHeader();
+	
+	$htmlout .= '<div id="title">' . $site->getWord('menu_home') . '</div>';
+	
+	$htmlout .= '<div id="content">';
+	
+	if ($site->getLanguage() == 'ro')
+	{
+		$htmlout .=
+		'<p>Acest site conține toate informațiile legate de clubul de poker FileList.
+		Aici puteți găsi tot: de la numărul de puncte al unui jucător, până la evoluția în timp a
+		numărului de participanți a turneelor; de la statistici legate de premii și bonus-uri până la
+		totalul punctelor cheltuite de către toți membrii.</p>
+		<p>Puteți răsfoi în voie. Enjoy.</p>';
+	}
+	else if ($site->getLanguage() == 'en')
+	{
+		$htmlout .=
+		'<p>This site contains all the informations regarding the FileList poker club.
+		You can find everything here: the number of points of a player, the evolution in time of the
+		total number of players registered in a tournament. You can even find statistics about bonuses,
+		prizes and the total number of points spent by the club members.</p>
+		<p>You may browse to any page. Enjoy.</p>';
+	}
+	
+	$htmlout .= '</div>';
+	
+	$htmlout .= $site->getFooter();
+
+	$htmlout .= '</body></html>';
+	
+	echo $htmlout;
+
+
+
+	
+//the general template looks like this: (presented just as guideline):
+
+/*
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,8 +64,8 @@
 		</ul>
 		
 		<p id="language_panel">
-			<a href="lang_switch.php?lang=ro"><img class="active_lang" src="images/ro.gif" title="Switch to Romanian" /></a>
-			<a href="lang_switch.php?lang=en"><img src="images/us.gif" title="Switch to English" /></a>
+			<a href="lang_switch.php?lang=ro"><img  src="images/ro.gif" title="Switch to Romanian" /></a>
+			<a href="lang_switch.php?lang=en"><img class="active_lang" src="images/us.gif" title="Switch to English" /></a>
 		</p>
 		
 		<div id="title">
@@ -40,3 +84,5 @@
 	</div>
 </body>
 </html>
+
+ */
--- lang_switch.php
+++ lang_switch.php
@@ -1,11 +1,11 @@
 <?php
-	require_once 'config/site_config.php';
+require_once 'config/site.config.php';
 	
-	if ($_GET['lang'] !== 'ro' && $_GET['lang'] !== 'en')
-	{
-		header('Location: index.php');
-	}
-	
-	setcookie('sitelang', $_GET['lang'], $siteConfig['lang_cookie_duration']);
-	
+if ($_GET['lang'] !== 'ro' && $_GET['lang'] !== 'en')
+{
 	header('Location: index.php');
+}
+	
+setcookie($siteConfig['lang_cookie_name'], $_GET['lang'], time() + $siteConfig['lang_cookie_duration']);
+	
+header('Location: index.php');
