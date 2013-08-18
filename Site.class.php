d442b1ffbf02f4069ffea5fc868445acd68a6325
Radu Murzea <radu.murzea@gmail.com>
2013-08-18 19:35:43 +0300

    version v0.93

--- Site.class.php
+++ Site.class.php
@@ -174,7 +174,7 @@ class Site
 	public function getFooter ()
 	{
 		$out = '<div id="footer">
-			FileList Poker Points v0.9.
+			FileList Poker Points v0.93.
 			<br />
 			Copyright &copy; 2013 Radu Murzea.
 			<br />
