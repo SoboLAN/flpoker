26b644b8a31af7dc15cec27cff6d64f88b7cf653
Radu Murzea <radu.murzea@gmail.com>
2013-09-03 21:25:56 +0300

    Added form for inserting tournament

---
+++ add.tournament.execute.php
@@ -0,0 +1,50 @@
+<?php
+
+if (! isset($_POST['flpokerpassword']) or $_POST['flpokerpassword'] !== 'myfladminpass8uhb')
+{
+	die ('nice try.');
+}
+
+if (! isset ($_POST['tournamentdate']) or
+	! isset ($_POST['participants']) or
+	! isset ($_POST['hours']) or
+	! isset ($_POST['minutes']))
+{
+	die ('Some data is missing.');
+}
+
+require_once 'DB.class.php';
+
+$db = Database::getConnection()->getPDO ();
+
+try
+{
+	$insertSt = $db->prepare (
+		'INSERT INTO tournaments (tournament_id, tournament_date, tournament_type, participants, ' .
+		'duration_hours, duration_minutes) ' .
+		'VALUES' .
+		'(NULL, ?, \'regular\', ?, ?, ?)');
+	
+	$insertSt->bindParam (1, $_POST['tournamentdate'], PDO::PARAM_STR);
+	$insertSt->bindParam (2, $_POST['participants'], PDO::PARAM_INT);
+	$insertSt->bindParam (3, $_POST['hours'], PDO::PARAM_INT);
+	$insertSt->bindParam (4, $_POST['minutes'], PDO::PARAM_INT);
+	
+	$insertSt->execute ();
+
+	if ($insertSt->rowCount () !== 1)
+	{
+		die('There was an error while adding the tournament');
+	}
+	
+	$getIdStatement = $db->prepare ('SELECT MAX(tournament_id) AS tournament_id FROM tournaments');
+	$getIdStatement->execute ();
+	
+	$id = $getIdStatement->fetch (PDO::FETCH_OBJ)->tournament_id;
+}
+catch (PDOException $e)
+{
+	die ('There was an error while executing the script: ' . $e->getMessage());
+}
+
+echo "Added tournament with ID $id ({$_POST['tournamentdate']}).";
---
+++ add.tournament.form.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>FileList Poker Add Tournament</title>
+ 
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
+<link rel="stylesheet" href="css/dark-hive/jquery-ui-1.10.3.custom.min.css" />
+ 
+<style type="text/css">
+	div{
+		padding:8px;
+	}
+	
+	div label {
+		padding: 8px;
+	}
+	
+	body {
+		background:#BCD5E1;
+	}
+</style>
+ 
+</head>
+
+<body>
+ 
+<h1>FileList Poker Add Tournament</h1>
+ 
+<script type="text/javascript">
+ 
+$(document).ready(function()
+{
+	$('#tournamentdate').datepicker ({dateFormat: 'yy-mm-dd'});
+});
+</script>
+
+<form action="add.tournament.execute.php" method="POST" target="_blank">
+	<div>
+		<label>Tournament Date: </label>
+		<input type="text" name="tournamentdate" id="tournamentdate" value="" />
+	</div>
+	<div>
+		<label>Participants: </label>
+		<input type="text" name="participants" id="participants" value="" />
+	</div>
+	<div>
+		<label>Duration Minutes: </label>
+		<input type="text" name="hours" id="hours" value="" />
+	</div>
+	<div>
+		<label>Duration Hours: </label>
+		<input type="text" name="minutes" id="minutes" value="" />
+	</div>
+	<p>
+		<label>Password: </label>
+		<input type='password' name='flpokerpassword' id='flpokerpassword' value ='' />
+	</p>
+	<input type='submit' value='Submit' id='submitbutton' />
+</form>
+<h2 style="font-weight:bold; color: red">WARNING: This cannot be undone. Be VERY SURE before submitting.</h2>
+
+</body>
+</html>
