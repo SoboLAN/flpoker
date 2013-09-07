858c451c6ece063081a1f201abd12e42c866e3a8
Radu Murzea <radu.murzea@gmail.com>
2013-09-07 15:29:56 +0300

    Added a cleanup cache script. This will work as a cron job in production, deleting once a month cache entries that are older than 4 days

---
+++ cleanup.cache.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'DB.class.php';
+
+$db = Database::getConnection()->getPDO();
+
+try
+{
+	//delete cache entries older than 4 days
+	$age = 4 * 24 * 3600;
+	$result = $db->prepare ('DELETE FROM cache WHERE UNIX_TIMESTAMP(NOW()) - entry_time > ?');
+	$result->bindParam (1, $age, PDO::PARAM_INT);
+	$result->execute ();
+}
+catch (PDOException $e)
+{
+	die('There was a problem while performing database queries');
+}
