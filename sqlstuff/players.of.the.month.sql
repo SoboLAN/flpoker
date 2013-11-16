a0a8c118caabfee6a5f412fc377fceb1ca0b67f8
Radu Murzea <radu.murzea@gmail.com>
2013-11-16 13:26:19 +0200

    Some cleanup in the sqlstuff folder

--- sqlstuff/players.of.the.month.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO players_of_the_month (player_of_the_month_id, player_id, award_month, award_year)
-VALUES
-(NULL, 3, 1, 2013),
-(NULL, 11, 2, 2013),
-(NULL, 111, 3, 2013),
-(NULL, 3, 4, 2013),
-(NULL, 12, 5, 2013),
-(NULL, 25, 6, 2013),
-(NULL, 10, 7, 2013);
