0ebcfce38d78ec6d3f1b21c3c7a2fde0e314329e
Radu Murzea <radu.murzea@gmail.com>
2013-11-30 20:12:32 +0200

    Implemented new template functionality on the index page

--- index.php
+++ index.php
@@ -6,10 +6,8 @@ use FileListPoker\Main\FLPokerException;
 try {
     $site = new Site();
 
-    $htmlout = $site->getHeader();
+    $htmlout = $site->getFullPageTemplate('index.php');
 
-    $htmlout .= '<div id="title">' . $site->getWord('menu_home') . '</div>
-                <div id="content">';
 } catch (FLPokerException $ex) {
     switch ($ex->getType()) {
         case FLPokerException::ERROR:
@@ -25,7 +23,7 @@ try {
 
 if ($site->getLanguage() == 'ro')
 {
-    $htmlout .=
+    $text =
     '<p>Acest site conține toate informațiile legate de clubul de poker FileList.
     Aici puteți găsi tot: de la numărul de puncte al unui jucător, până la evoluția în timp a
     numărului de participanți a turneelor; de la statistici legate de premii și bonus-uri până la
@@ -34,69 +32,18 @@ if ($site->getLanguage() == 'ro')
 }
 else if ($site->getLanguage() == 'en')
 {
-    $htmlout .=
+    $text =
     '<p>This site contains all the informations regarding the FileList poker club.
     You can find everything here: the number of points of a player, the evolution in time of the
     total number of players registered in a tournament. You can even find statistics about bonuses,
     prizes and the total number of points spent by the club members.</p>
     <p>You may browse to any page. Enjoy.</p>';
 }
-    
-$htmlout .= '</div>';
-    
-$htmlout .= $site->getFooter();
 
-$htmlout .= '</body></html>';
-    
+$htmlout = str_replace(
+    array('{content_type_id}', '{page_content}', '{bottom_page_scripts}'),
+    array('content', $text, ''),
+    $htmlout
+);
+
 echo $htmlout;
-
-
-
-    
-//the general template looks like this: (presented just as guideline):
-
-/*
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <title>FileList Poker - Punctaje</title>
-    <link rel="shortcut icon" href="favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-</head>
-<body>
-    <div id="container">
-    
-        <ul class="claybricks">
-            <li><a href="page1.php" class="selected">Home Page</a></li>
-            <li><a href="page1.php">Classic View</a></li>
-            <li><a href="page1.php">Statistics</a></li>
-            <li><a href="page1.php">Players</a></li>
-            <li><a href="page1.php">Tournaments</a></li>
-            <li><a href="page1.php">Rules</a></li>
-            <li><a href="page1.php">Ask for Prize</a></li>
-        </ul>
-        
-        <p id="language_panel">
-            <a href="lang_switch.php?lang=ro"><img  src="images/ro.gif" title="Switch to Romanian" /></a>
-            <a href="lang_switch.php?lang=en"><img class="active_lang" src="images/us.gif" title="Switch to English" /></a>
-        </p>
-        
-        <div id="title">
-                Home
-        </div>
-        
-        <div id="content">
-
-            Welcome to FileList Poker Points website.
-        
-        </div>
-        
-        <div id="footer">
-            Just saying something.        
-        </div>
-    </div>
-</body>
-</html>
-
- */
